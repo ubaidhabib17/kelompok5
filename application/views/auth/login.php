d0627b7b1a4916de2b7432d7c1d30378bdab3421
kiki <rahmarizki37@gmail.com>
2020-10-07 21:36:32 +0700

    template register done

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -31,7 +31,7 @@
 										<a class="small" href="forgot-password.html">Forgot Password?</a>
 									</div>
 									<div class="text-center">
-										<a class="small" href="register.html">Create an Account!</a>
+										<a class="small" href="<?php echo base_url('auth/registration')?>">Create an Account!</a>
 									</div>
 								</div>
 							</div>
